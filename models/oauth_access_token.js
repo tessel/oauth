fa93ae1d9b9efaa83be05e1dec7efb365bacc613
edgarsilva <edgar.osc@gmail.com>
2014-04-24 18:08:25 -0500

    Rename oauth_access_tokens model to singular, add oauth_client model, updated model constraints

--- models/oauth_access_token.js
+++ models/oauth_access_token.js
@@ -1,7 +1,7 @@
 var OauthAccessToken = function(sequelize, DataTypes){
   var OauthAccessTokenModel = sequelize.define('OauthAccessToken', {
     id: { type: DataTypes.UUID, unique: true, primaryKey: true },
-    accessToken: { type: DataTypes.TEXT, allowNull: false },
+    accessToken: { type: DataTypes.TEXT, unique: true, allowNull: false },
     clientId: { type: DataTypes.TEXT, allowNull: false },
     userId: { type: DataTypes.STRING, allowNull: false },
     expires: { type: DataTypes.DATE, allowNull: false }
---
+++ models/oauth_client.js
@@ -0,0 +1,12 @@
+var OauthClient = function(sequelize, DataTypes){
+  var OauthAccessTokenModel = sequelize.define('OauthAccessToken', {
+    id: { type: DataTypes.UUID, unique: true, primaryKey: true },
+    clientId: { type: DataTypes.TEXT, unique:true, allowNull: false },
+    clientSecret: { type: DataTypes.TEXT, allowNull: false },
+    redirectURI: { type: DataTypes.TEXT, allowNull: false },
+  });
+
+  return OauthAccessTokenModel;
+};
+
+module.exports = OauthClient;
