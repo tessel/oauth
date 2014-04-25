f201455c7ce4ca842e0add35876f858e0b9663e8
Andrew Stewart <andrew@stwrt.ca>
2014-04-25 15:54:34 -0600

    Clean up app.js, contain all routes in object

--- app.js
+++ app.js
@@ -9,14 +9,17 @@ var express = require('express'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser');
 
-var routes = require('./routes/index'),
-    users = require('./routes/users'),
-    oauthRoutes = require('./routes/oauth2');
+var oauthserver = require('node-oauth2-server');
+
+var routes = {
+  index: require('./routes/index'),
+  users: require('./routes/users'),
+  oauth: require('./routes/oauth2')
+};
 
 var db = require('./models/db');
 
-var app = express(),
-    oauthserver = require('node-oauth2-server');
+var app = express();
 
 // Configure OAuth2 server
 app.oauth = oauthserver({
@@ -24,7 +27,6 @@ app.oauth = oauthserver({
   grants: ['password', 'authorization_code'],
   debug: true
 });
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -39,16 +41,16 @@ app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Use OAuth to grant tokens
-app.use('/oauth', oauthRoutes.all(app));
-app.use('/', routes);
-app.use('/users', users);
+app.use('/oauth', routes.oauth.all(app));
+app.use('/',      routes.index);
+app.use('/users', routes.users);
 
 // Setup and Sync Database and load models
 db.sequelize
   //.sync({ force: true })
   .sync()
   .complete(function(err){
-    if (err) {
+    if (!!err) {
       console.log(err);
       process.exit();
     }
