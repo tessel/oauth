65c8b89f3baf4c103ce308e910783cae929d294a
Andrew Stewart <andrew@stwrt.ca>
2014-04-24 18:19:09 -0600

    Clean up DB setup; use env vars for configuration

--- models/db.js
+++ models/db.js
@@ -1,19 +1,25 @@
-var Sequelize= require('sequelize'),
-    sequelize= new Sequelize('tessel-cloud', 'edgar', 'tessel', {
-      host: '127.0.0.1',
-      port: 5432,
-      dialect: 'postgres'
-    }),
-    db = {
-      Sequelize: Sequelize,
-      sequelize: sequelize
-    };
+var Sequelize= require('sequelize');
 
-// Require models
+var sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: '127.0.0.1',
+    port: 5432,
+    dialect: 'postgres'
+  }
+);
+
+var db = {
+  Sequelize: Sequelize,
+  sequelize: sequelize
+};
+
+// import models
 db['User'] = db.sequelize.import('./user');
 db['OauthClient'] = db.sequelize.import('./oauth_client');
 db['OauthAccessToken'] = db.sequelize.import('./oauth_access_token');
 db['OauthRefreshToken'] = db.sequelize.import('./oauth_refresh_token');
 
 module.exports = db;
-
