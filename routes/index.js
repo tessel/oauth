b6990b148a9bb35b88be5f4fd84e819d0553bfef
Andrew Stewart <andrew@stwrt.ca>
2014-05-01 11:23:04 -0700

    Refactor/cleanup Index routes

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,17 @@
-var express = require('express'),
-    router = express.Router(),
-    bcrypt = require('bcrypt'),
-    db = require('../models/db'),
-    index = {};
+var router = require('express').Router();
 
-index.all = function(oauth){
-  /* GET home page. */
-  router.get('/', oauth.authorise(), function(req, res) {
-    res.render('index', { title: 'Express' });
-  });
+module.exports = {
+  all: function(oauth) {
+    // GET /
+    router.get('/', oauth.authorise(), function(req, res) {
+      res.render('index', { title: 'Express' });
+    });
 
-  /* GET Register new user redirects to users/new. */
-  router.get('/register', function(req, res) {
-    res.redirect('/users/new');
-  });
+    // GET /register
+    router.get('/register', function(req, res) {
+      res.redirect('/users/new');
+    });
 
-  return router;
-};
-
-module.exports = index;
+    return router;
+  }
+}
