657aade3c967d4b4de26abe7770c3fe1352aec18
edgarsilva <edgar.osc@gmail.com>
2014-05-04 13:09:15 -0500

    Remove params logging.

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,6 @@ module.exports = {
   all: function(oauth) {
     // GET /
     router.get('/', oauth.authorise(), function(req, res) {
-      console.log('REQ.BODY =>', req.body);
-      console.log('REQ.QUERY =>', req.query);
       res.render('index', { title: 'Express' });
     });
 
