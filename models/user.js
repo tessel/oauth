fec9b83213141650b5b23fc57be6c4e199befbb2
Andrew Stewart <andrew@stwrt.ca>
2014-05-06 15:09:41 -0700

    Change API key generation
    Now uses a combination of the current time in MS and Math.random().
    
    32 char keys are generated, and existing NOT NULL constraint on DB field should
    prevent collisions.

--- models/user.js
+++ models/user.js
@@ -67,15 +67,16 @@ var User = function(sequelize, DataTypes){
       },
 
       genApiKey: function(){
-        var salt = bcrypt.genSaltSync(5);
+        var d = Date.now();
+        var apikey = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
 
-        var apiKey = bcrypt.hashSync(
-          Date.now().toString() +
-          this.username +
-          Math.random().toString()
-        , salt);
+        apikey = apikey.replace(/[xy]/g, function(c) {
+          var r = (d + Math.random() * 16) % 16 | 0;
+          d = Math.floor(d / 16);
+          return (c === 'x' ? r : (r&0x7|0x8)).toString(16);
+        });
 
-        this.apiKey = new Buffer(apiKey).toString('base64');
+        this.apiKey = apikey;
 
         return this;
       },
