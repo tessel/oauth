0ec2953f4ee05688a620f17dfb50c49596ab97a4
Nathan Vander Wilt <nate@calftrail.com>
2014-10-30 16:48:34 -0700

    add dev env seed

--- seeds/clients.js
+++ seeds/clients.js
@@ -31,3 +31,19 @@ db.Client
   .error(function(err){
     console.log('error ====>', err);
   })
+
+db.Client
+  .create({
+    clientId: 'tessel-cloud-dev',
+    clientSecret: 'the-tessel-devenv',
+    clientSecretConfirmation: 'the-tessel-devenv',
+    name: 'http://127.0.0.1:3000/login',
+    grantTypeAllowed: 'password|authorization_code|refresh_token|https://tessel-grant',
+    redirectUri: 'http://127.0.0.1:3000/oauth_redirect'
+  })
+  .success(function(client){
+    console.log('client ====>', client);
+  })
+  .error(function(err){
+    console.log('error ====>', err);
+  })
