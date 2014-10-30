c3b9615552d7b469c7308d78cc3a5734bed68328
Nathan Vander Wilt <nate@calftrail.com>
2014-10-30 14:53:52 -0700

    attempt to make GOOGLE/GITHUB settings optional. this may have side effects later, but seems to work around #30 at least as far as loading the page

--- controllers/application_controller.js
+++ controllers/application_controller.js
@@ -57,7 +57,7 @@ passport.deserializeUser(function(obj, done) {
   done(null, obj);
 });
 
-passport.use(new GoogleStrategy({
+if (process.env.GOOGLE_CLIENT_ID) passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.GOOGLE_CALLBACK_URL
@@ -94,7 +94,7 @@ passport.use(new GoogleStrategy({
   }
 ));
 
-passport.use(new GitHubStrategy({
+if (process.env.GITHUB_CLIENT_ID) passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
     callbackURL: process.env.GITHUB_CALLBACK_URL
