72f9d0002cf6b4aa59c81dfc62841669e2dd0072
Nathan Vander Wilt <nate@calftrail.com>
2015-01-20 17:01:19 -0800

    add sample proxy client to seed

--- seeds/clients.js
+++ seeds/clients.js
@@ -47,3 +47,19 @@ db.Client
   .error(function(err){
     console.log('error ====>', err);
   })
+
+db.Client
+  .create({
+    clientId: 'tessel-proxy-dev',
+    clientSecret: 'the-tessel-devenv',
+    clientSecretConfirmation: 'the-tessel-devenv',
+    name: 'http://127.0.0.1:3000/login',
+    grantTypeAllowed: 'https://tessel-grant',
+    redirectUri: 'http://127.0.0.1:3000/oauth_redirect'
+  })
+  .success(function(client){
+    console.log('client ====>', client);
+  })
+  .error(function(err){
+    console.log('error ====>', err);
+  })
